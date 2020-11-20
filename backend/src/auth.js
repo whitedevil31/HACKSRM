85ceafb9061b2d8de12d520cd5b1db5485796ce3
whitedevil31 <magz3116@gmail.com>
2020-11-21 00:18:55 +0530

    auth and routes created

---
+++ backend/src/auth.js
@@ -0,0 +1,26 @@
+const User = require("./model/userSchema");
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token;
+    req.user = user;
+
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate." });
+  }
+};
+
+module.exports = auth;
