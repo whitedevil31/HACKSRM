5db94096b77695f99fd66e416302e0a20d65439c
whitedevil31 <magz3116@gmail.com>
2020-11-21 07:36:04 +0530

    frontEnd routes added

--- FrontEnd/sharetrip/src/App.js
+++ FrontEnd/sharetrip/src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Login from "./Route/Login";
+import SignUp from "./Route/SignUp";
+import DashBoard from "./Route/DashBoard";
+import CreatePost from "./Route/CreatePost";
+import PostId from "./Route/PostId";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>hello world</h1>
-      
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/users">
+          <SignUp />
+        </Route>
+        <Route path="/dashboard">
+          <DashBoard />
+        </Route>
+        <Route path="/post">
+          <CreatePost />
+        </Route>
+        <Route path="/postId">
+          <PostId />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
