ce5d49e28d5b6e4e873e0216e20b48c42841f0bb
whitedevil31 <magz3116@gmail.com>
2020-11-22 00:34:10 +0530

    moment added

--- frntEnd/sharetrip/src/Route/Login/Login.js
+++ frntEnd/sharetrip/src/Route/Login/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   const [spinner, setSpinner] = useState(false);
   const { register, handleSubmit } = useForm();
   const OnSubmit = (data) => {
-    fetch("", {
+    fetch("https://travel-partner-backend.herokuapp.com/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
