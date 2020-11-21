f6e3d95181d0793e4fecbf14e0cd179c8da72bf6
whitedevil31 <magz3116@gmail.com>
2020-11-21 15:12:49 +0530

    user logout added

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -31,5 +31,17 @@ router.post("/users/login", async (req, res) => {
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+    console.log(e);
+  }
+});
 
 module.exports = router;
